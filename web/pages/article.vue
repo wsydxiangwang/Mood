1de4b44a44bc5ec986b8948edb160378db55b0fa
Libai <1915398623@qq.com>
2020-02-25 08:41:18 +0800

    2

--- web/pages/article.vue
+++ web/pages/article.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="container">
         <section class="list">
-            <div v-for="(value, key, index) in datas" :key="index">
-                <h2>{{key}}</h2>
+            <div v-for="(val, key, idx) in list" :key="idx">
+                <h2 class="year">{{key}} 年</h2>
+                <div class="mon-list" v-for="(vals, keys, idxs) in val">
+                    <h3 class="month">{{keys}} 月</h3>
+                    <ul>
+                        <li v-for="(valss, keyss, idxss) in vals">
+                            <span>{{valss.time.day}}日：</span>
+                            <span>{{valss.title}}</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     </div>
@@ -12,71 +21,19 @@
 export default {
     data(){
         return{
-            datas: {
-                '2020': {
-                    '11': [
-                        {
-                            time: '12',
-                            name: '余生，我们一起吃饭'
-                        },
-                        {
-                            time: '25',
-                            name: '余生，我们一起吃饭'
-                        }
-                    ],
-                    '08': [
-                        {
-                            time: '04',
-                            name: '余生，我们一起吃饭'
-                        },
-                        {
-                            time: '08',
-                            name: '余生，我们一起吃饭'
-                        }
-                    ]
-                },
-                '1299': {
-                    '11': [
-                        {
-                            time: '12',
-                            name: '余生，我们一起吃饭'
-                        },
-                        {
-                            time: '25',
-                            name: '余生，我们一起吃饭'
-                        }
-                    ],
-                    '08': [
-                        {
-                            time: '04',
-                            name: '余生，我们一起吃饭'
-                        },
-                        {
-                            time: '08',
-                            name: '余生，我们一起吃饭'
-                        }
-                    ]
-                },
-                
-            }
+            list: ''
         }
     },
     mounted(){
-
-        console.log(this.data)
-
-        this.data.reduce((a,b)=>{
-            console.log(a)
-            console.log(b)
-            var [,year,date] = /(\d+)\/(\d+\/\d+)/.exec(b.time.date);
+        this.list = this.data.reduce((a,b)=>{
+            var [,year,date] = /(\d+)\/(\d+)/.exec(b.time.date);
             a[year] = a[year] || {};
             a[year][date] = a[year][date] || [];
             a[year][date].push(b);
             return a;
         }, {})
 
-        console.log(this.data)
-
+        console.log(this.list)
     },
     async asyncData(context){
 		let {data} = await context.$axios.get('article')
@@ -86,5 +43,31 @@ export default {
 </script>
 
 <style lang="scss" scoped> 
-    
+.list{
+    width: 800px;
+    margin: 0 auto;
+    .year{
+        color: #333;
+        font-size: 22px;
+        font-weight: 400;
+        margin-top: 20px;
+    }
+    .mon-list{
+        margin-top: 10px;
+        .month{
+            color: #333;
+            font-weight: 400;
+        }
+        ul{
+            margin-top: 10px;
+            padding-left: 25px;
+            li{
+                margin: 10px 0;
+                span:last-child{
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+} 
 </style>
