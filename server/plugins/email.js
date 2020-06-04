9a239313fdc41faf6af403579917ee0656cdfa1a
Libai <1915398623@qq.com>
2020-06-04 11:45:25 +0800

    邮件

--- server/plugins/email.js
+++ server/plugins/email.js
@@ -27,7 +27,7 @@ function emailFn(item){
                                     <h2 style="font-size: 16px;font-weight: 400;font-style: oblique;font-size:font-size: 1rem;">hi，${name}，你今天笑了么～</h2>
                                     <p style="text-indent: 2em;color:#303030;font-size: 0.9rem;line-height: 24px;">偷偷告诉你一件事，您在《<a href="${item.url}">${item.title}</a>》的心情中，收到一条新的回复啦，赶紧<a href="${item.url}">回来看看</a>是哪位神仙眼光这么好，竟然选到了世界上最棒的人～</p>
                                     <p style="text-align: right;margin-top: 40px;font-size:0.9rem">—— 白茶</p>
-                                    <div style="background: #eff5fb;border-left: 4px solid #c2e1ff;padding: 14px;margin-top: 30px;border-radius: 9px;font-size: 0.85rem;color: #7d7f7f;line-height: 24px;">如果我们没有机会见面，那我在这儿提前预祝你早安、午安以及晚安<br>愿你所见皆彩虹，所遇皆良人，所求皆所愿，所盼皆所期<br>接受所有完美和不完美，永远相信美好的事情即将发生～～</div>
+                                    <div style="background: #eff5fb;border-left: 4px solid #c2e1ff;padding: 14px;margin-top: 30px;border-radius: 9px;font-size: 0.85rem;color: #7d7f7f;line-height: 24px;">如果我们没有机会见面，那我在这儿提前预祝你早安、午安以及晚安～～<br>愿所有的美好如约而至，愿所有的黑暗都能看到希望，我们微笑前行～～<br>人生没有完美，也许有些遗憾才美～～永远相信美好的事情即将发生～～</div>
                                 </div>
                             </td>
                         </tr>
@@ -50,14 +50,8 @@ function sendMail(data, call) {
         subject: data.title + ' | 白茶',
         html: data.content,
     };
-    transporter.sendMail(mailOptions, (error, response) => {
-        if (error) {
-            call(error)
-        } else {
-            call('Email sentd')
-        }
-    });
-    transporter.close(); // 如果没用，关闭连接池
+    transporter.sendMail(mailOptions, (error, response) => error ? call(error) : call('Email sentd'));
+    transporter.close(); // 关闭连接池
 }
 
 module.exports = emailFn
