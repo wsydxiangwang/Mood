4b0757e7e01a9bb483c35c524f5ddb7302161f36
Libai_mac <1915398623@qq.com>
2020-07-01 08:56:08 +0800

    admin comment view

--- admin/src/views/comment.vue
+++ admin/src/views/comment.vue
@@ -126,7 +126,7 @@ export default {
         },
         // 新窗口打开文章
         view(id){
-            window.open(`${window.location.host}/${id}`)
+            window.open(`${window.location.origin}/${id}`)
         },
         remove(item){
            
